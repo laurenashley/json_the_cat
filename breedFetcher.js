794487b58d7d341d1e75702396e99a8046255003
Lauren Ashley <la.zaubi@gmail.com>
2022-10-16 16:45:34 -0700

    fetching cat breed object and printing breed desc using hard coded bread data

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,1 +1,15 @@
+// breedFetcher.js
 
+const request = require('request');
+const api = 'https://api.thecatapi.com/v1/breeds/search?q=';
+
+const fetchBreedData = (breed) => {
+  const resource = api + breed;
+
+  request(resource, (error, response, body) => {
+    const data = JSON.parse(body);
+    console.log(`Description of the ${breed} cat breed: ${data[0]['description']}`);
+  });
+};
+
+fetchBreedData('Siberian');
