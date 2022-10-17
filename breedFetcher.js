d7c2aad0e635fe50cff1d2d13f008cf137373eda
Lauren Ashley <la.zaubi@gmail.com>
2022-10-16 17:05:58 -0700

    handles if missing breed arg or breed not found

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,25 @@
 // breedFetcher.js
 
 const request = require('request');
-const breed = process.argv.slice(2);
+const args = process.argv.slice(2);
 const api = 'https://api.thecatapi.com/v1/breeds/search?q=';
+const breed = args[0];
 
 const fetchBreedData = (breed) => {
   const resource = api + breed;
 
-  request(resource, (error, response, body) => {
+  return request(resource, (error, response, body) => {
     const data = JSON.parse(body);
-    console.log(`Description of the ${breed} cat breed: ${data[0]['description']}`);
+    if (data[0] === undefined) {
+      console.log('Breed name was not found.');
+    } else {
+      console.log(`Description of the ${breed} cat breed: ${data[0]['description']}`);
+    }
   });
 };
-
-fetchBreedData(breed);
+if (breed === undefined) {
+  console.log('Please provide a breed name.');
+  return false;
+} else {
+  fetchBreedData(breed);
+}
