64b8c1d5e4f3abbe2b3b266eec0cc3edd76c9867
Lauren Ashley <la.zaubi@gmail.com>
2022-10-16 18:17:49 -0700

    still need to handle error if url is invalid

--- breedFetcher.js
+++ breedFetcher.js
@@ -8,14 +8,18 @@ const breed = args[0];
 const fetchBreedData = (breed) => {
   const resource = api + breed;
 
-  return request(resource, (error, response, body) => {
-    const data = JSON.parse(body);
-    if (data[0] === undefined) {
-      console.log('Breed name was not found.');
-    } else {
-      console.log(`Description of the ${breed} cat breed: ${data[0]['description']}`);
-    }
-  });
+  try {
+    return request(resource, (error, response, body) => {
+      const data = JSON.parse(body);
+      if (data[0] === undefined) {
+        console.log('Breed name was not found.');
+      } else {
+        console.log(`Description of the ${breed} cat breed: ${data[0]['description']}`);
+      }
+    });
+  } catch (err) {
+    console.log('ERROR: ', err);
+  }
 };
 if (breed === undefined) {
   console.log('Please provide a breed name.');
